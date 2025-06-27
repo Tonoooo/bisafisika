c8b0ca9d6e19b59eedfa5f4f9427720c7273aba1
tonoooo <adomtonot@gmail.com>
2025-06-27 15:18:31 +0700

    scrool for mobile view

--- resources/views/filament/pages/quiz-history.blade.php
+++ resources/views/filament/pages/quiz-history.blade.php
@@ -7,7 +7,7 @@
                 </h2>
             </div>
 
-            <div class="overflow-hidden border border-gray-200 dark:border-gray-700 sm:rounded-lg">
+            <div class="overflow-x-auto border border-gray-200 dark:border-gray-700 sm:rounded-lg">
                 <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                     <thead class="bg-gray-50 dark:bg-gray-800">
                         <tr>
